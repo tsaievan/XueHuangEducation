eea6b0e630cf4629ee116816e738225b61f15fb3
tsaievan <tsaievan@icloud.com>
2018-04-30 14:27:22 +0800

    把几个tableView都加载上去了

--- XueHuangEducation/Classes/HomePage/Controllers/XHCatalogListViewController.swift
+++ XueHuangEducation/Classes/HomePage/Controllers/XHCatalogListViewController.swift
@@ -11,16 +11,45 @@ import UIKit
 ///< 这里的逻辑有点多, 直接将view写在控制器里, 方便调用各个方法, 免得产生太多的胶水代码
 class XHCatalogListViewController: XHBaseViewController {
     
+    var teachVc: XHTeachViewController = XHTeachViewController()
+    
+    var themeVc: XHThemeViewController = XHThemeViewController()
+    
+    var questionVc: XHQuestionViewController = XHQuestionViewController()
+    
     lazy var segmentView: XHCatalogListSegmentView = {
         let s = XHCatalogListSegmentView()
-        s.backgroundColor = .cyan
+        s.backgroundColor = .white
         return s
+    }()
+    
+    lazy var scrollView: UIScrollView = {
+        let sv = UIScrollView()
+        sv.backgroundColor = .yellow
+        sv.contentSize = CGSize(width: SCREEN_WIDTH * 3, height: 0)
+        sv.showsVerticalScrollIndicator = false
+        sv.showsHorizontalScrollIndicator = false
+        sv.bounces = false
+        sv.isPagingEnabled = true
+        sv.addSubview(contentView)
+        return sv
+    }()
+    
+    lazy var contentView: UIView = {
+        let cv = UIView()
+        cv.addSubview(teachVc.view)
+        cv.addSubview(themeVc.view)
+        cv.addSubview(questionVc.view)
+        return cv
     }()
     
     
     ///< 生命周期
     override func viewDidLoad() {
         super.viewDidLoad()
+        addChildViewController(teachVc)
+        addChildViewController(themeVc)
+        addChildViewController(questionVc)
         setupUI()
     }
 
@@ -31,6 +60,7 @@ extension XHCatalogListViewController {
     fileprivate func setupUI() {
         view.backgroundColor = .white
         view.addSubview(segmentView)
+        view.addSubview(scrollView)
         makeConstraints()
     }
     
@@ -40,5 +70,39 @@ extension XHCatalogListViewController {
             make.left.right.equalTo(view)
             make.height.equalTo(84)
         }
+        
+        scrollView.snp.makeConstraints { (make) in
+            make.top.equalTo(segmentView.snp.bottom)
+            make.left.right.bottom.equalTo(view)
+        }
+        
+        teachVc.view.snp.makeConstraints { (make) in
+            make.top.left.bottom.equalTo(contentView)
+            make.width.equalTo(SCREEN_WIDTH)
+        }
+        
+        themeVc.view.snp.makeConstraints { (make) in
+            make.top.bottom.width.equalTo(teachVc.view)
+            make.left.equalTo(teachVc.view.snp.right)
+        }
+        
+        questionVc.view.snp.makeConstraints { (make) in
+            make.top.bottom.width.equalTo(themeVc.view)
+            make.left.equalTo(themeVc.view.snp.right)
+        }
+        
+        ///< 上下锁定, 左右对齐
+        contentView.snp.makeConstraints { (make) in
+            make.top.equalTo(segmentView.snp.bottom)
+            make.bottom.equalTo(view)
+            make.left.right.equalTo(scrollView)
+            make.width.equalTo(SCREEN_WIDTH * 3)
+        }
+    }
+}
+
+extension XHCatalogListViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        
     }
 }
