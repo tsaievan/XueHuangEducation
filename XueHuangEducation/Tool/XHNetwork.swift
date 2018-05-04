39e1bcec392ac2fe3413c7e3dbece5f059bd5799
tsaievan <tsaievan@icloud.com>
2018-05-04 17:25:47 +0800

    有一个坑, 要设置cookie过期时间

--- XueHuangEducation/Tool/XHNetwork.swift
+++ XueHuangEducation/Tool/XHNetwork.swift
@@ -14,6 +14,14 @@ typealias FailueBlock = (Error) -> ()
 
 
 class XHNetwork {
+    
+    /// get请求, 得到的response是json字符串
+    ///
+    /// - Parameters:
+    ///   - url: 请求的url地址
+    ///   - params: 请求的参数
+    ///   - success: 请求成功的回调
+    ///   - failue: 请求失败的回调
     class func GET(url: String, params: [String : Any]?, success: SuccessBlock?, failue: FailueBlock?) {
         ///< 发送请求的时候要把cookie的值拼接到header发送给服务器
         var cookieDict = [String : String]()
@@ -40,8 +48,30 @@ class XHNetwork {
         }
     }
     
+    /// get请求, 得到的response是非json字符串
+    ///
+    /// - Parameters:
+    ///   - url: 请求的url地址
+    ///   - params: 请求参数
+    ///   - success: 请求成功的回调
+    ///   - failue: 请求失败的回调
     class func GET_ResponseString(url: String, params: [String : Any]?, success: SuccessBlock?, failue: FailueBlock?) {
-        Alamofire.request((URL_BASE as NSString).appendingPathComponent(url), method: .get, parameters: params, encoding: URLEncoding.default, headers: nil).responseString { (response) in
+        ///< 发送请求的时候要把cookie的值拼接到header发送给服务器
+        var cookieDict = [String : String]()
+        let cookieJar =  HTTPCookieStorage.shared
+        var cookieHeader = HTTPHeaders()
+        if let cookies = cookieJar.cookies {
+            for cookie in cookies {
+                cookieDict[cookie.name] = cookie.value
+            }
+            var cookieString = ""
+            for (k, v) in cookieDict {
+                let string = String(format: "%@=%@;", k, v)
+                cookieString = cookieString.appending(string)
+            }
+            cookieHeader["Cookie"] = cookieString
+        }
+        Alamofire.request((URL_BASE as NSString).appendingPathComponent(url), method: .get, parameters: params, encoding: URLEncoding.default, headers: cookieHeader).responseString { (response) in
             switch response.result {
             case .success(let value):
                 success?(value)
