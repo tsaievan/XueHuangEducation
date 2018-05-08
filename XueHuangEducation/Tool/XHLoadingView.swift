15134fbe596029f7c716404bd88240d70c470507
tsaievan <tsaievan@icloud.com>
2018-05-08 14:02:10 +0800

    写一个loadingView, 加载起来好看些

---
+++ XueHuangEducation/Tool/XHLoadingView.swift
@@ -0,0 +1,14 @@
+//
+//  XHLoadingView.swift
+//  XueHuangEducation
+//
+//  Created by tsaievan on 8/5/18.
+//  Copyright © 2018年 tsaievan. All rights reserved.
+//
+
+import UIKit
+
+class XHLoadingView: UIView {
+
+    
+}
